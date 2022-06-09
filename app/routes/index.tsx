996262ae926008537e2435ffe0c77ec24bdfa763
Chakib Benchinoun <chakib.ben022@gmail.com>
2022-06-09 21:59:50 +0100

    feat: setup home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,8 @@
 export default function Index() {
-  return <div className="text-4xl font-bold underline text-red-500"></div>
+  return (
+    <div className="relative h-screen overflow-x-hidden">
+      <div className="bg-blue-50 h-[120%] w-[90%] -top-[480px] -right-[450px] -rotate-[24deg] absolute -z-50" />
+      <div className="lg:max-w-7xl xl:px-10 p-3 mx-auto mt-20"></div>
+    </div>
+  )
 }
